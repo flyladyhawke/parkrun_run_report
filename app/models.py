87081ab4f09ab65b6699159220bab9dad446a788
Unknown <flyladyhawke@gmail.com>
2018-08-05 22:35:21 +1000

    Add models

---
+++ app/models.py
@@ -0,0 +1,54 @@
+from app import db
+
+
+class Option(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String(50), index=True)
+    value = db.Column(db.String(50))
+    description = db.Column(db.String(150))
+
+    def __repr__(self):
+        return '<Option {} {}>'.format(self.key, self.description)
+
+
+class Section(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    description = db.Column(db.String(150))
+    sections = db.relationship('EventSection', backref='section', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Section {}>'.format(self.description)
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    event_name = db.Column(db.String(50), index=True)
+    event_number = db.Column(db.Integer, index=True)
+    sections = db.relationship('EventSection', backref='event', lazy='dynamic')
+    options = db.relationship('EventOption', backref='event', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Event {} {}>'.format(self.event_name, self.event_number)
+
+
+class EventOption(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
+    key = db.Column(db.String(50), index=True)
+    value = db.Column(db.String(50))
+
+
+class EventSection(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
+    section_id = db.Column(db.Integer, db.ForeignKey('section.id'))
+    order = db.Column(db.Integer)
+    display = db.Column(db.Integer)
+
+
+class EventSectionText(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    event_section_id = db.Column(db.Integer, db.ForeignKey('event_section.id'))
+    part = db.Column(db.String(50))
+    text = db.Column(db.String(250))
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, url_for
-from app import app
+from app import app, db
 from app.forms import CreateForm
 from src import run_report_utils
+from app.models import Event
 
 
 @app.route('/')
@@ -14,8 +15,30 @@ def index():
 @app.route('/create', methods=['GET', 'POST'])
 def create():
     form = CreateForm()
+    result = ''
+    if form.validate_on_submit():
+        model = Event()
+        form.populate_obj(model)
+        db.session.add(model)
+        db.session.commit()
+
+        run_report = run_report_utils.RunReportWeek(form.event_name.data, form.event_number.data)
+        result = {'links': run_report.print_urls(form.week_number.data, 8)}
+
+    return render_template('create.html', title='Create', form=form, result=result)
+
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    form = CreateForm()
     if form.validate_on_submit():
         run_report = run_report_utils.RunReportWeek(form.event_name.data, form.event_number.data)
         result = {'links': run_report.print_urls(form.week_number.data, 8)}
-        return render_template('create.html', title='Sign In', form=form, result=result)
-    return render_template('create.html', title='Sign In', form=form)
+        return render_template('admin/admin.html', title='Admin', form=form, result=result)
+    return render_template('admin/admin.html', title='Admin', form=form)
+
+
+@app.route('/event', methods=['GET', 'POST'])
+def events():
+    current = Event.query.all()
+    return render_template('events.html', title='Events', current=current)
